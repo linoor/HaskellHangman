e334117d575003c282b4d3b8079f05afc08f7f7f
Michał Pomarański <michal.pomaranski@gmail.com>
2014-12-07 16:13:30 +0100

    game is working

--- Hangman.hs
+++ Hangman.hs
@@ -5,16 +5,25 @@ import System.Random
 import Data.Char
 
 hangman :: String -> [Char] -> IO ()
-hangman word guessed
-	| all (`elem` guessed) word = putStrLn "won"
-	| length guessed > 5        = putStrLn ("Lost, the word was: " ++ word)
+hangman word lettersUsed
+	| all (`elem` lettersUsed) word        = putStrLn ("won, the word was: " ++ word)
+	| (length $ lettersMissed word lettersUsed) > 5    = putStrLn ("Lost, the word was: " ++ word)
 	| otherwise = do
+		putStrLn ("the word: " ++ (hide word lettersUsed))
+
+		putStrLn ("letters missed: " ++ lettersMissed word lettersUsed)
+
 		l <- toLower . head <$> prompt "Pick a letter:"
-		let guessed'
-			| l `elem` guessed = guessed
-			| otherwise        = l:guessed
-		putStrLn ("guessed = " ++ guessed)
-		hangman word guessed'
+
+		let lettersUsed'
+			| l `elem` lettersUsed = lettersUsed
+			| otherwise            = l:lettersUsed
+
+		if (length lettersUsed') > 0
+			then putStrLn ("lettersUsed = " ++ [x | x <- lettersUsed', not $ x `elem` word])
+			else return ()
+
+		hangman word lettersUsed'
 
 main :: IO ()
 main = do
@@ -23,7 +32,13 @@ main = do
 	chosenWords	<- filter ((l==) . length) <$> map init <$> lines <$> readFile "./enable1.txt"
 	-- choosing one word from the list
 	chosenWord <- (!!) chosenWords <$> randomRIO (0, length chosenWords)
-	-- game
+	-- start game loop
 	hangman chosenWord []
 
 prompt s = putStr (s ++ " ") >> getLine
+
+hide :: String -> String -> String
+hide word guesses = map (\x -> if x `elem` guesses then x else '-') word
+
+lettersMissed :: String -> String -> [Char]
+lettersMissed word letters = [x | x <- letters, not $ x `elem` word]
